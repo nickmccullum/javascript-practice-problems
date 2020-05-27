19cb364395acaa97293716f771d013afd186e49a
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-05-27 12:01:43 -0300

    added boolean practice problems

---
+++ starter-files/04-booleans.js
@@ -0,0 +1,43 @@
+//Before starting these practice problems, you will want to connect your index.html file to this JavaScript file.
+//To do this, change the script tag so that it links to 'starter-files/04-booleans.js' instead of 'script.js'.
+
+// Question 1
+//Create boolean variables called trueBoolean and falseBoolean and assign them values of true and false, respectively.
+//For the reasons discussed in the course, be sure to uset he let keyword.
+
+
+
+//Question 2
+//Using the var1 and var2 variables declared below, 
+//log a boolean value to the console that indicates whether var1 is greater than var2.
+
+const var1 =  5;
+const var2 = 10;
+
+//Solution goes here
+
+
+
+//Question 3
+//Log a boolean value to the console that indicates whether var1 is less than var2.
+
+
+
+//Question 4
+//Log a boolean value to the console that indicates whether var1 is greater than or equal to var2.
+
+
+
+//Question 5
+//Log a boolean value to the console that indicates whether var1 is less or equal to var2.
+
+
+
+//Question 6
+//Given the language variable declared below, 
+//log a boolean value to the console that tests whether this language is equal to "Python"
+
+const language = 'JavaScript';
+
+//Solution goes here
+
