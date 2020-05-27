86e140598901a579871a0f925bcf6bf6cbcf20e9
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-05-27 11:45:09 -0300

    added solutions to numbers practice problems

---
+++ finished-files/03-numbers-FINISHED.js
@@ -0,0 +1,67 @@
+//Before starting these practice problems, you will want to connect your index.html file to this JavaScript file.
+//To do this, change the script tag so that it links to 'starter-files/03-numbers.js' instead of 'script.js'.
+
+// Question 1
+//Create a JavaScript number variable called year. Use the const keyword. 
+//Assign it a value equal to the current calendar year.
+
+const year = 2020;
+
+//Question 2
+//Given var1 and var2 below, divide var1 by var2. Log this value to the console.
+
+const var1 = 15;
+const var2 = 30;
+
+//Solution goes here
+
+console.log(var1/var2);
+
+//Question 3
+//Add var1 and var2 together. Log this value to the console.
+
+console.log(var1 + var2);
+
+//Question 4
+//Subtract var1 from var2. Log this value to the console.
+
+console.log(var2 - var1);
+
+//Question 5
+//Multiply var1 and var2 together. Log this value to the console.
+
+console.log(var1 * var2);
+
+//Question 6
+//Raise var2 to the power of var1. Log this value to the console.
+
+console.log(var2 ** var1);
+
+//Question 7
+//Calculate the remainder when var2 is divided by 9. Log this value to the console.
+
+console.log(var2 % 9);
+
+//Question 8
+//Round 2.2 down to the nearest integer. Log this value to the console.
+
+console.log(Math.floor(2.2));
+
+//OR
+
+console.log(Math.round(2.2));
+
+//Question 9
+//Round 2.9 up to the nearest integer. Log this value to the console.
+
+console.log(Math.ceil(2.9));
+
+//OR
+
+console.log(Math.round(2.9));
+
+//Question 10
+//Generate a random number between 0 and 100 using the Math module.
+//Log this number to the console.
+
+console.log(Math.random()*100);
