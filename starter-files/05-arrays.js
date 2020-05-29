459a7994a4a26372549113aaf5cdf8c0f6d8621c
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-05-29 08:27:02 -0300

    Create 05-arrays.js

---
+++ starter-files/05-arrays.js
@@ -0,0 +1,105 @@
+//Before starting these practice problems, you will want to connect your index.html file to this JavaScript file.
+//To do this, change the script tag so that it links to 'starter-files/05-arrays.js' instead of 'script.js'.
+
+// Question 1
+//Create a JavaScript arrays that contains two elements. 
+//The elements should be your first and last name.
+//Assign this array to a variable named myNames.
+
+
+
+//Question 2
+//Log the second element of this array to the console.
+
+
+
+//Question 3
+//Calculate the length of this array and log it to the console.
+
+
+
+//Question 4
+//Access the last item of the following JavaScript array and log it to the console.
+
+longArray = [1, 2, 3, 7, 6, 9, 7, 5, 3, 4, 7, 9, 6, 1, 5, 8];
+
+//Solution goes here
+
+
+
+//Question 5
+//Depending on your opinion of this course so far,
+//Add either "best" or "worst" to the end of the following array.
+//Log the new array to the console.
+
+const sentenceArray = ['This', 'course', 'is', 'the'];
+
+//Solution goes here
+
+
+
+
+//Question 6
+//Add 10 to the start of the following array.
+//Log the new array to the console.
+
+const multiplesOfTen = [20, 30, 40, 50];
+
+//Solution goes here
+
+
+
+
+//Question 7
+//Reverse the order of the following array.
+//Assign the reversed array to a new variable called reversedArray.
+//Log reversedArray to the console.
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//Solution goes here:
+
+
+
+
+//Question 8
+//Use the slice method to select mushrooms and onions from the following array.
+//Assign these two items to a new array called someIngredients.
+//Log someIngredients to the console.
+
+const allIngredients = ['pepperoni', 'sauce', 'flour', 'mushrooms', 'onions', 'green peppers', 'cheese'];
+
+//Solution goes here
+
+
+
+
+//Question 9
+//What is the main difference between the slice and splice methods?
+
+
+
+//Question 10
+//Use the spread operator combined with the slice method to add'PEI' between 'Nova Scotia' and 'New Brunswick'.
+//Assign the new array to a variable called maritimeProvinces.
+//Log this array to the console.
+
+const someProvinces = ['Nova Scotia', 'New Brunswick'];
+
+//Solution goes here
+
+
+
+//Question 11
+//Use the spread operator and the slice method to remove 'ethane' from the following array.
+//Store the new array in a variable called someHydrocarbons
+//Log the new array to the console.
+
+const allHydrocarbons = ['methane', 'ethane', 'propane', 'butane', 'pentane', 'hexane', 'heptane', 'octane', 'nonane', 'decane'];
+
+//Solution goes here
+
+
+
+//Question 12
+//Create a copy of the allHydrocarbons array and store it in a variable called hydrocarbonCopy
