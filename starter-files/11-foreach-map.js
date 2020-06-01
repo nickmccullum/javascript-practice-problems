13ca91aebb259177706edcfaaee705d9af8dcd98
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-06-01 11:57:58 -0300

    Create 11-foreach-map.js

---
+++ starter-files/11-foreach-map.js
@@ -0,0 +1,54 @@
+//Before starting these practice problems, you will want to connect your index.html file to this JavaScript file.
+//To do this, change the script tag so that it links to 'starter-files/11-foreach-map.js' instead of 'script.js'.
+
+//Question 1
+//Use the forEach method to log every element of the following array to the console.
+//Do not create a separate function for this.
+
+const programmingLanguages = ['JavaScript', 'Python', 'Java', 'Go']
+
+//Solution goes here
+
+
+
+//Question 2
+//Create a function called logFirstCharacter that accepts a string 
+//and logs the string's first character to the console.
+//Apply the function to every item in the programmingLanguages array using the forEach method.
+
+
+
+//Question 3
+//Create a function called logLastCharacter that accepts a string 
+//and logs the string's last character to the console.
+//Apply the function to every item in the programmingLanguages array using the forEach method.
+
+
+
+//Question 4
+//Given the following array of numbers, use an arrow function inside of 
+//the forEach method to add 7 to every item in the array.
+//Log the modified array to the console after the forEach method is complete. 
+//Note that the original array should not be modified.
+
+const numberArray = [5,2,3,81,5,4,7,8,9,6];
+
+//Solution goes here
+
+
+
+//Question 5
+//Using the map method instead of the forEach method, 
+//log the modified array (where each item is increased by 7) to the console.
+
+
+
+//Question 6
+//Using the map method, an arrow function, and the lowercaseNames array defined below,
+//create a new array called uppercaseNames that has the same strings but with all uppercase letters.
+//Log uppercaseNames to the console.
+//HINT: You'll need the toUpperCase method.
+
+const lowercaseNames = ['Jonathan', 'Neal', 'Stephen', 'Ross'];
+
+//Solution goes here
