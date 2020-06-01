7409aca3595ee44bf4b59394e231188a37f76e54
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-06-01 12:03:05 -0300

    Create 12-filter-find.js

---
+++ finished-files/12-filter-find.js
@@ -0,0 +1,43 @@
+//Before starting these practice problems, you will want to connect your index.html file to this JavaScript file.
+//To do this, change the script tag so that it links to 'starter-files/12-filer-find.js' instead of 'script.js'.
+
+//Question 1
+//Use the JavaScript filter function to return an array whose elements 
+//are all the elements of numberArray that are greater than 6.
+//Log this new array to the console.
+
+numberArray = [1, 2, 3, 7, 9, 4, 6, 8, 5, 1, 6, 4, 7, 9, 6]
+
+//Solution goes here
+
+console.log(numberArray.filter(num => num > 6));
+
+//Question 2
+//In the lesson associated with these practice problems, we saw the following array:
+
+const journal = [
+
+    {events: ["work", "touched tree", "pizza",
+  
+              "running", "television"],
+  
+     squirrel: false},
+  
+    {events: ["work", "ice cream", "cauliflower",
+  
+              "lasagna", "touched tree", "brushed teeth"],
+  
+     squirrel: false},
+  
+    {events: ["weekend", "cycling", "break", "peanuts",
+  
+              "beer"],
+  
+     squirrel: true},
+  
+  ];
+
+//Use the find method to return the first day where the author did not encounter a squirrel.
+//Log this journal entry to the console.
+
+console.log(journal.find(entry => entry.squirrel === false));
