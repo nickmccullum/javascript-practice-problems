0bead97807020b334270f8998487e82ed550b925
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-06-01 09:40:41 -0300

    added solution to loops

---
+++ finished-files/09-loops-FINISHED.js
@@ -0,0 +1,58 @@
+//Before starting these practice problems, you will want to connect your index.html file to this JavaScript file.
+//To do this, change the script tag so that it links to 'starter-files/09-loops.js' instead of 'script.js'.
+
+//Question 1
+//Write a for loop that logs the integers from 0 to 4 (inclusive) to the console.
+
+for (let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+//Question 2
+//Write a for loop that logs every character of the following string to the console.
+
+sentence = 'This JavaScript course is awesome!';
+
+//Solution goes here
+
+for(let i = 0; i < sentence.length; i++){
+    console.log(sentence[i]);
+}
+
+//Question 3
+//Write a 'for of' loop that logs every element of the following array to the console.
+
+myArray = [5, 6, 7, 8];
+
+//Solution goes here
+
+for (item of myArray){
+    console.log(item);
+}
+
+//Question 4
+//Using the name variable defined below, use a 'for in' loop to print the integers from 0 to 3 (inclusive).
+
+name = 'Nick';
+
+//Solution goes here
+
+for (index in name){
+    console.log(index);
+}
+
+//Question 5
+//Write a 'while' loop that adds every integer from 0 to 3 (inclusive) to the sum variable defined below.
+//Log the sum variable to the console once the loop is complete.
+
+let sum = 0;
+
+//Solution goes here
+
+let i = 0;
+while (i <= 3){
+    sum += i;
+    i++;
+}
+
+console.log(sum);
